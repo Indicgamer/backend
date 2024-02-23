e06b4cbd11f2f9748adfd54aea3f1de91f975c74
indic_gamer <channayyagarampalli@gmmail.com>
2024-02-23 21:27:14 +0530

    added a simple session login functionality

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require("express");
+const session = require("express-session");
 
 const app = express();
+
+//middleware
+app.use(session({
+    secret:"secret",
+    resave:false,
+    saveUninitialized:false
+    }));
+
 
 app.get("/",(req,res)=>{
     res.send("Hello World");
 })
+
+app.get("/login",(req,res)=>{
+    if(req.session.isLoggedIn){
+        res.send("You are already logged in");
+    }
+    req.session.isLoggedIn = true;
+    res.send("Logged in");
+})
+
 
 
 app.listen(3000,()=>{
