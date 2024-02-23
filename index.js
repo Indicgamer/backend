40d4eb813aa7f6edff01f40fd74df0a6e4473a94
indic_gamer <channayyagarampalli@gmmail.com>
2024-02-23 20:50:55 +0530

    simple express app

--- index.js
+++ index.js
@@ -1,38 +1,12 @@
-const express = require('express');
-const session = require('express-session');
+const express = require("express");
 
-const PORT = 3000;
 const app = express();
 
-//middlewares
-app.use(express.json());
-app.use(session({
-    secret:"MySecret",
-    resave:false,
-    saveUninitialized:false
-    }));
-
-
-
-
-const validateSession = (req, res, next) => {
-    if(req.session.isLoggedIn){
-        next();
-    }else{
-        res.status(401).json({message:"Unauthenticated"});
-    }
-}
-
-//routes
-app.use("/",(req,res)=>{
+app.get("/",(req,res)=>{
     res.send("Hello World");
 })
-app.use('/api/auth', require('./routes/authRouter'));
-app.use("/api/prod", validateSession,require('./routes/prodRouter'));
 
 
-
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
 });
