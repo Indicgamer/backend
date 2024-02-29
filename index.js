9847bd4c7dd2c7a1ba5e0146ce142f6b4aa66121
indic_gamer <channayyagarampalli@gmmail.com>
2024-02-29 10:51:38 +0530

    mongoDB check

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const session = require("express-session");
+const mongoose = require("mongoose");
 
 const app = express();
+(async() =>{
+    try{
+        await mongoose.connect("mongodb+srv://indic_gamer:<password>@cluster0.3gah2fh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        console.log("Connected to the database");
+    }catch(e){
+        console.log("MongoDB error: ",e);
+    }
+})();
+
 
 //middleware
 app.use(session({
@@ -9,19 +19,63 @@ app.use(session({
     resave:false,
     saveUninitialized:false
     }));
+app.use(express.json());
 
 
 app.get("/",(req,res)=>{
     res.send("Hello World");
 })
 
-app.get("/login",(req,res)=>{
+
+const users = mongoose.Schema({
+    username:String,
+    password:String
+});
+
+const usersModel = mongoose.model("users",users);
+
+
+
+app.post("/register",(req,res)=>{
     if(req.session.isLoggedIn){
         res.send("You are already logged in");
     }
-    req.session.isLoggedIn = true;
-    res.send("Logged in");
+    const [username,password] = [req.body.username,req.body.password];
+    const user = usersModel({
+        username:username,
+        password:password
+    });
+    user.save().then(()=>{
+        res.send("User registered successfully");
+        req.session.isLoggedIn = true;
+    }).catch((e)=>{
+        res.status(500).send("Error: "+e);
+    });
 })
+
+app.get("/users",(req,res)=>{
+    if(!req.session.isLoggedIn){
+        res.send("You are not logged in");
+    }
+    usersModel.find().then((users)=>{
+        res.send(users);
+    }).catch((e)=>{
+        res.status(500).send("Error: "+e);
+    });
+});
+
+app.delete("/users/:username",(req,res)=>{
+    if(!req.session.isLoggedIn){
+        res.send("You are not logged in");
+    }
+    usersModel.deleteOne({username:req.params.username}).then(()=>{
+        res.send("User deleted successfully");
+    }).catch((e)=>{
+        res.status(500).send("Error: "+e);
+    });
+});
+
+
 
 
 
