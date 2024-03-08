b40fc0e360c01f7d5cd7f7baf5eb49b609e5cc96
indic_gamer <channayyagarampalli@gmmail.com>
2024-03-08 12:49:52 +0530

    fucked cors and cookie

--- index.js
+++ index.js
@@ -7,11 +7,18 @@ const app = express();
 
 //middlewares
 app.use(express.json());
+let thirtyDays = 1000 * 60 * 60 * 24 * 30; //30 days worth of milliseconds
 app.use(session({
     secret:"MySecret",
     resave:false,
     saveUninitialized:false,
-    }));
+    cookie: {
+        maxAge: thirtyDays,
+        sameSite: "none",
+        secure: true,
+    }
+}));
+
 app.use(cors({
     origin: true,
     credentials: true,
